a36036d10a43c9f95302a8e5e750dc6150af486a
C. Titus Brown <titus@idyll.org>
2012-06-05 01:58:46 +0000

    fixed PATH in run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-source ~/.bashrc
 exec > /tmp/ipynb.out 2> /tmp/ipynb.errout
 
 cd /usr/local/notebooks
@@ -7,4 +6,5 @@ cd /usr/local/notebooks
 #git checkout -f master
 #git pull origin master
 
+export PATH=/usr/local/bin:$PATH
 /usr/local/bin/ipython notebook --certfile=~/.ipython/mycert.pem --pylab inline --no-browser --ip=* --port=443
